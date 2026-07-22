1c5f5abc49e11fe46bca4a82cb3917cb61371cc4
Hasintha Attanayake <hasintha.attanayake@gmail.com>
2026-07-22 22:03:50 +0800

    NEM-003: chore: add test project + gh actions test workflow

---
+++ NEM.Model.Tests/DemandTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace NEM.Model.Tests
+{
+    public class DemandTests
+    {
+        [Fact]
+        public void Demand_ShouldSetProperties_WhenCreated()
+        {
+            // Arrange
+            var time = DateTimeOffset.Now;
+            var megawatts = 100.5;
+
+            // Act
+            var demand = new Demand(time, megawatts);
+
+            // Assert
+            demand.Time.Should().Be(time);
+            demand.Megawatts.Should().Be(megawatts);
+        }
+    }
+}
